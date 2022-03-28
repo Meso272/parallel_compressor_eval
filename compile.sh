78931611c4317ffda11b869e29fefc147141559a
meso272 <liujinyang1201@126.com>
2022-03-28 16:13:11 -0700

    111

--- compile.sh
+++ compile.sh
@@ -19,10 +19,10 @@ gcc -c rw.c
 
 # mpicc -std=c99 -O3 rw.o parallel_sz2.c -o parallel_sz2 -I$szsrc $szlib/libSZ.a $szlib/libzstd.a $szlib/libzlib.a -lm
 
-mpicc -std=c99 -O3 rw.o parallel_sz2.c -o parallel_sz2 -I$szsrc -L$szlib -lSZ -lzstd -lm
+mpicc -std=c99 -O3 rw.o parallel_sz2.c -o parallel_sz2 -I $szsrc -L $szlib -l SZ -l zstd -lm
 
-mpicc -std=c99 -O3 rw.o parallel_sz3.c -o parallel_sz3 -I$sz3src -L$sz3lib -lSZ -lzstd -lm
+mpicc -std=c99 -O3 rw.o parallel_sz3.c -o parallel_sz3 -I $sz3src -L $sz3lib -l SZ -l zstd -lm
 
-mpicc -std=c99 -O3 rw.o parallel_zfp.c -o parallel_zfp -I$zfpsrc -L$zfplib -lzfp -lm
+mpicc -std=c99 -O3 rw.o parallel_zfp.c -o parallel_zfp -I $zfpsrc -L $zfplib -l zfp -lm
 
-mpicc -std=c99 -O3 rw.o parallel_mgard.c -o parallel_mgard -I$mgardsrc   -lm
+mpicc -std=c99 -O3 rw.o parallel_mgard.c -o parallel_mgard -I $mgardsrc   -lm
