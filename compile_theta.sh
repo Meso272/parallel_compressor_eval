9191b1f0bcc654f6687f72aa3865952ac39491d3
meso272 <liujinyang1201@126.com>
2022-03-30 16:46:06 -0700

    111

--- compile_theta.sh
+++ compile_theta.sh
@@ -27,7 +27,7 @@ g++ -c rw.c -o rwx.o
 
 # mpicc -std=c99 -O3 rw.o parallel_sz2.c -o parallel_sz2 -I$szsrc $szlib/libSZ.a $szlib/libzstd.a $szlib/libzlib.a -lm
 
-cc -std=c99 -O3 rw.o parallel_sz2_theta.c -o parallel_sz2 -I $szsrc -L $szlib  -l SZ -I $zstdsrc -L $zstdlib -l zlib -l zstd -lm
+cc -std=c99 -O3 parallel_sz2_theta.c -o parallel_sz2 -I $szsrc -L $szlib  -l SZ -I $zstdsrc -L $zstdlib -l zlib -l zstd -lm
 
 CC  -O3 rwx.o parallel_sz3_theta.c -o parallel_sz3 -I $sz3src -L $sz3lib  -I $zstdsrc -L $zstdlib -l zstd -lm
 
