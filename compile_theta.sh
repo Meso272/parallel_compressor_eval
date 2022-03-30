6ba8388b0b953b7fdb7851dec88fc37785b9c933
meso272 <liujinyang1201@126.com>
2022-03-30 16:22:52 -0700

    111

--- compile_theta.sh
+++ compile_theta.sh
@@ -27,15 +27,15 @@ g++ -c rw.c -o rwx.o
 
 # mpicc -std=c99 -O3 rw.o parallel_sz2.c -o parallel_sz2 -I$szsrc $szlib/libSZ.a $szlib/libzstd.a $szlib/libzlib.a -lm
 
-mpicc -std=c99 -O3 rw.o parallel_sz2_theta.c -o parallel_sz2 -I $szsrc -L $szlib  -l SZ -I $zstdsrc -L $zstdlib -l zlib -l zstd -lm
+cc -std=c99 -O3 rw.o parallel_sz2_theta.c -o parallel_sz2 -I $szsrc -L $szlib  -l SZ -I $zstdsrc -L $zstdlib -l zlib -l zstd -lm
 
-mpicxx  -O3 rwx.o parallel_sz3_theta.c -o parallel_sz3 -I $sz3src -L $sz3lib  -l zstd -lm
+CC  -O3 rwx.o parallel_sz3_theta.c -o parallel_sz3 -I $sz3src -L $sz3lib  -l zstd -lm
 
-mpicxx  -O3 rwx.o parallel_sz3_theta.c -o parallel_qoz -I $qozsrc -L $qozlib -l zstd -lm 
-
-
+CC  -O3 rwx.o parallel_sz3_theta.c -o parallel_qoz -I $qozsrc -L $qozlib -l zstd -lm 
 
 
-mpicc -std=c99 -O3 rw.o parallel_zfp_theta.c -o parallel_zfp -I $zfpsrc -L $zfplib -l zfp -lm
 
-mpicxx  -O3 rwx.o parallel_mgard_theta.c -o parallel_mgard -I $mgardsrc  -I $metasrc -L $metalib -I $zstdsrc -L $zstdlib -l sz_cpp -l zstd -lm
+
+cc -std=c99 -O3 rw.o parallel_zfp_theta.c -o parallel_zfp -I $zfpsrc -L $zfplib -l zfp -lm
+
+CC  -O3 rwx.o parallel_mgard_theta.c -o parallel_mgard -I $mgardsrc  -I $metasrc -L $metalib -I $zstdsrc -L $zstdlib -l sz_cpp -l zstd -lm
