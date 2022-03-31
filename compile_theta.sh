94907681b33aa3b22644a8da3fa89b49b7bd0ed0
meso272 <liujinyang1201@126.com>
2022-03-30 17:12:17 -0700

    111

--- compile_theta.sh
+++ compile_theta.sh
@@ -38,4 +38,5 @@ g++ -c rw.c -o rwx.o
 
 #cc -std=c99 -O3 rw.o parallel_zfp_theta.c -o parallel_zfp -I $zfpsrc -L $zfplib -l zfp -lm
 echo $HOME/packages/SZcpp/lib64
+ls $HOME/packages/SZcpp/lib64
 CC -O3 rwx.o parallel_mgard_theta.c -o parallel_mgard -I $zstdsrc -L $zstdlib  -l zstd -I $mgardsrc  -I $metasrc -L $metalib    -l sz_cpp  -lm
