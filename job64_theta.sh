9bb3f9befbe253da7d485b9e62d79f1a439e1cdc
meso272 <liujinyang1201@126.com>
2022-03-30 19:18:51 -0700

    111

--- job64_theta.sh
+++ job64_theta.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+source ~/.bashrc
 echo wait .... 30 seconds
 sleep 30
 echo date=`date`
@@ -7,18 +8,18 @@ cd $HOME/lossycompression/parallel_compressor_eval
 # Hurricane
 aprun -n 64 ./parallel_sz2 13 496 496 96
 aprun -n 64 ./parallel_zfp 13 496 496 96
-#aprun -n 16384 ./parallel_mgard 13 496 496 96
+aprun -n 64 ./parallel_mgard 13 496 496 96
 aprun -n 64 ./parallel_sz3 hurricane.config 13 496 496 96
-printf "SZ3 END"
+printf "SZ3 END\n"
 srun -n 64 ./parallel_qoz hurricane.config 13 496 496 96
-printf "QOZ END"
+printf "QOZ END\n"
 
 # miranda
 aprun -n 64 ./parallel_sz2  7 384 384 256
 aprun -n 64 ./parallel_zfp 7 384 384 256
-#srun ./parallel_mgard 7 384 384 256
+aprun -n 64 ./parallel_mgard 7 384 384 256
 aprun -n 64 ./parallel_sz3 miranda.config 7 384 384 256
-printf "SZ3 END"
+printf "SZ3 END\n"
 aprun -n 64 ./parallel_qoz miranda.config 7 384 384 256
-printf "QOZ END"
+printf "QOZ END\n"
 
