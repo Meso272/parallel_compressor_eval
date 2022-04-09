384265aefc86764bf19326b45d2f7d133f89ec94
meso272 <liujinyang1201@126.com>
2022-04-09 14:44:21 -0700

    111

--- job32_salt.sh
+++ job32_salt.sh
@@ -5,29 +5,17 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node=32
 #SBATCH --time=5:00:00
-#SBATCH -o p32.%j.%N.out
-#SBATCH -e p32.%j.%N.error
+#SBATCH -o p32salt.%j.%N.out
+#SBATCH -e p32salt.%j.%N.error
 
 echo wait .... 30 seconds
 sleep 30
 echo date=`date`
 
 
-# Hurricane
-srun ./parallel_sz 13 496 496 96
-srun ./parallel_zfp 13 496 496 96
-srun ./parallel_mgard 13 496 496 96
-srun ./parallel_sz3 hurricane.config 13 496 496 96
-printf "SZ3 END"
-srun ./parallel_qoz hurricane.config 13 496 496 96
-printf "QOZ END"
+# Salt
 
-# miranda
-srun ./parallel_sz  7 384 384 256
-srun ./parallel_zfp 7 384 384 256
-srun ./parallel_mgard 7 384 384 256
-srun ./parallel_sz3 miranda.config 7 384 384 256
-printf "SZ3 END"
-srun ./parallel_qoz miranda.config 7 384 384 256
-printf "QOZ END"
+srun ./parallel_qoz salt.config 51 368 1010 1010
+#printf "QOZ END"
+
 
